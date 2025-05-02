e84ec197fd09543fe4b68fbc09b48eb856c4d393
HeyVds <vitorsan642@gmail.com>
2025-05-02 20:28:53 -0300

    prova 01

---
+++ 01/index.js
@@ -0,0 +1,9 @@
+const n1 = 7;
+const n2 = 8;
+const n3 = 9;
+const p1 = 1;
+const p2 = 2;
+const p3 = 2;
+
+let media = (n1 * p1 + n2 * p2 + n3 * p3) / (p1 + p2 + p3);
+console.log("A média é:", media);
